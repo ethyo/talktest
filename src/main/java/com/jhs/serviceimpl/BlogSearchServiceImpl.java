104c88c7640df1b305e3ce25d524974e777282d6
DESKTOP-5L6PFEQ\kjw <jhs@ubline.com>
2023-03-22 14:48:28 +0900

    handling exception case

--- src/main/java/com/jhs/serviceimpl/BlogSearchServiceImpl.java
+++ src/main/java/com/jhs/serviceimpl/BlogSearchServiceImpl.java
@@ -5,9 +5,9 @@ import com.jhs.entity.UserString;
 import com.jhs.repository.UserStringRepository;
 import com.jhs.service.BlogSearchService;
 import com.jhs.util.JavaOpenKoreanTextProcessor;
-import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.lucene.util.ArrayUtil;
 import org.openkoreantext.processor.phrase_extractor.KoreanPhraseExtractor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
@@ -24,6 +24,7 @@ import java.time.LocalDateTime;
 import java.util.*;
 
 import com.google.gson.Gson;
+import scala.annotation.meta.param;
 
 @Service
 @Slf4j
@@ -41,72 +42,100 @@ public class BlogSearchServiceImpl implements BlogSearchService {
     public Mono<ServerResponse> getList(ServerRequest request) {
         Mono<MultiValueMap<String, String>> formData = request.formData();
         Gson gson = new Gson();
-        return formData
+
+        return checkParams(formData)
+                .onErrorReturn(new HashMap())
+                .doOnNext(param -> {
+                    log.debug("request param size: " + param.size());
+                })
                 .map(data -> {
-                    Map<String, String> dataMap = data.toSingleValueMap();
-                    UserString us = UserString.builder().userstring(dataMap.get("query")).createddate(LocalDateTime.now()).build();
-                    userStringRepository.save(us);
-                    return dataMap;
+                    if(data.size() != 0) {
+                        UserString us = UserString.builder().userstring(data.get("query").toString()).createddate(LocalDateTime.now()).build();
+                        userStringRepository.save(us);
+                        return data;
+                    }else {
+                        return new HashMap();
+                    }
                 })
                 .flatMap(data -> {
-                    // 형태소가 분석된 토큰 추출
-                    String userInputString = data.get("query");
-                    List<KoreanPhraseExtractor.KoreanPhrase> text = JavaOpenKoreanTextProcessor.getProcessedInput(userInputString);
-                    StringBuilder sb = new StringBuilder();
-                    for (KoreanPhraseExtractor.KoreanPhrase koreanPhrase : text) {
-                        log.debug("token: " + koreanPhrase.text() + "\n");
-                        sb.append(koreanPhrase.text() + " ");
+                    if(data.size() != 0) {
+                        // 형태소가 분석된 토큰 추출
+                        String userInputString = data.get("query").toString();
+                        List<KoreanPhraseExtractor.KoreanPhrase> text = JavaOpenKoreanTextProcessor.getProcessedInput(userInputString);
+                        StringBuilder sb = new StringBuilder();
+                        for (KoreanPhraseExtractor.KoreanPhrase koreanPhrase : text) {
+                            log.debug("token: " + koreanPhrase.text() + "\n");
+                            sb.append(koreanPhrase.text() + " ");
+                        }
+
+                        data.put("query", sb.toString()); // 형태소 분석된 키워드로 query 스트링 생성
+                        log.debug("query string: " + sb);
+
+                        String response = this.getRequest(KAKAO_BLOG_SEARCH_URI, data);
+                        Map successMap = gson.fromJson(response, HashMap.class);
+                        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(successMap);
+                    }else {
+                        Map<Object, Object> failMap = new HashMap<>();
+                        return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(failMap);
                     }
-
-                    data.put("query", sb.toString()); // 형태소 분석된 키워드로 query 스트링 생성
-                    log.debug("query string: " + sb);
-
-                    String response = this.postRequest(KAKAO_BLOG_SEARCH_URI, data);
-                    Map res = gson.fromJson(response, HashMap.class);
-                    return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).bodyValue(res);
-                })
-                .switchIfEmpty(Mono.error(new NotFoundException("no param")));
-
+                });
     }
 
-    public String postRequest(String pURL, Map<String, String> pList) {
+    private Mono<Map> checkParams(Mono<MultiValueMap<String, String>> params) {
+        String[] sort_strings = {"accuracy", "recency"};
+        List<String> valid_sort_strings = Arrays.asList(sort_strings);
 
+        // 필수 값 확인
+        return params.flatMap(item -> {
+            Map<String, String> pItem = item.toSingleValueMap();
+
+            String page = pItem.get("page");
+            if(page != null) {
+                if(!page.chars().allMatch(Character::isDigit)) {
+                    return Mono.error(new Exception("page param is not valid, input number type"));
+                }
+            }
+
+            if(!"".equals(pItem.get("query")) && pItem.get("query") != null) {
+                String sort = pItem.get("sort");
+                if(valid_sort_strings.contains(sort)) {
+                    pItem.put("query", pItem.get("query"));
+                    return Mono.just(pItem);
+                }else {
+                    return Mono.error(new Exception("sort param is not valid"));
+                }
+            }else {
+                return Mono.error(new Exception("query is empty"));
+            }
+        });
+    }
+
+    public String getRequest(String pURL, Map<String, String> pList) {
         String myResult = "";
 
         try {
-            //   URL 설정하고 접속하기
-            URL url = new URL(pURL); // URL 설정
+            //   URL 설정
+            URL url = new URL(pURL);
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
 
-            HttpURLConnection http = (HttpURLConnection) url.openConnection(); // 접속
-            //--------------------------
-            //   전송 모드 설정 - 기본적인 설정
-            //--------------------------
+            //전송 모드 설정 - 기본적인 설정
             http.setDefaultUseCaches(false);
-            http.setDoInput(true); // 서버에서 읽기 모드 지정
-            http.setDoOutput(true); // 서버로 쓰기 모드 지정
-            http.setRequestMethod("POST"); // 전송 방식은 POST
+            http.setDoInput(true);
+            http.setDoOutput(true);
+            http.setRequestMethod("GET");
 
-            //--------------------------
-            // 헤더 세팅
-            //--------------------------
-            // 서버에게 웹에서 <Form>으로 값이 넘어온 것과 같은 방식으로 처리하라는 걸 알려준다
+            //헤더 세팅
             http.setRequestProperty("content-type", "application/x-www-form-urlencoded");
-            //http.setRequestProperty("Authorization", "KakaoAK 0606458087ba27b68f726181e3a79be3");
             http.setRequestProperty("Authorization", headstring + " " + issuancekey);
 
-
-            //--------------------------
-            //   서버로 값 전송
-            //--------------------------
+            //서버로 값 전송
             StringBuffer buffer = new StringBuffer();
 
             //HashMap으로 전달받은 파라미터가 null이 아닌경우 버퍼에 넣어준다
             if (pList != null) {
-
-                Set key = pList.keySet();
-
-                for (Iterator iterator = key.iterator(); iterator.hasNext();) {
-                    String keyName = (String) iterator.next();
+                Set<String> key = pList.keySet();
+                for (Iterator<String> iterator = key.iterator(); iterator.hasNext();) {
+                    String keyName = iterator.next();
                     String valueName = pList.get(keyName);
                     if(iterator.hasNext()) {
                         buffer.append(keyName).append("=").append(valueName).append("&");
@@ -121,9 +150,7 @@ public class BlogSearchServiceImpl implements BlogSearchService {
             writer.write(buffer.toString());
             writer.flush();
 
-            //--------------------------
-            //   서버에서 전송받기
-            //--------------------------
+            // 응답 처리
             InputStreamReader tmp = new InputStreamReader(http.getInputStream(), "UTF-8");
             BufferedReader reader = new BufferedReader(tmp);
             StringBuilder builder = new StringBuilder();
